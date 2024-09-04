71e12c07970e7e8ce423f91d4d888aea39eaf757
Rajesh <peketirajesh007@gmail.com>
2024-09-04 19:00:58 +0530

    cors aadded

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*",
+        origin: "https://peer-server-s7xw.onrender.com",
         methods: ["GET", "POST"],
     }
 });
