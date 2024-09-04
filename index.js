33fbf9ef8a21cc132bbda9c7e3a5dad21527bf77
Rajesh <peketirajesh007@gmail.com>
2024-09-04 18:47:05 +0530

    socket added

--- index.js
+++ index.js
@@ -2,15 +2,38 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIo(server);
+
 const peerServer = ExpressPeerServer(server, {
     debug: true,
-    alive_timeout: true,
+    allow_discovery: true
+});
+
+let connectedPeers = new Set();
+
+peerServer.on('connection', (peerId) => {
+    connectedPeers.add(peerId.getId());
+    io.emit('peer-list-update', Array.from(connectedPeers));
+});
+
+peerServer.on('disconnect', (peerId) => {
+    connectedPeers.delete(peerId.getId());
+    io.emit('peer-list-update', Array.from(connectedPeers));
 });
 
 app.use('/peerjs', peerServer);
+
+io.on('connection', (socket) => {
+    socket.emit('peer-list-update', Array.from(connectedPeers));
+
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
